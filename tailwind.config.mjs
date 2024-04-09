acc26db594abcfffc6fd3da82522a9e128929681
ינוק <u9268782@bsmch.net>
2024-04-09 12:10:07 +0300

    changes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,17 @@
 import typography from "@tailwindcss/typography";
+import colors from "tailwindcss/colors";
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "selector",
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    fontFamily: {
+      sans: ["Raleway"],
+    },
+    colors: {
+      gray: colors.neutral,
+    },
     extend: {},
   },
   plugins: [typography],
