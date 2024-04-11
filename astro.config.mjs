89d1b91597d9389a18a178d5c29cca5404d9b9e7
yinon <yinongilad4@gmail.com>
2024-04-11 20:54:24 +0300

    blastoff

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,6 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   integrations: [],
+  site: 'https://yinon4.github.io/planet',
+  // base: 'planet'
 });
