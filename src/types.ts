8a61b6662d2bf2a6a90a2a2c3521c9ff49ea2006
yinon <yinongilad4@gmail.com>
2024-04-12 21:30:45 +0300

    books log and zod

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,25 @@
+import { z } from "astro/zod";
+
 export type Link = {
     href: string;
     text: string;
 };
+
+const datestring = z.string().refine((val) => Date.parse(val), { message: "Invalid datestring" })
+
+
+export const BookType = z.object({
+    title: z.string(),
+    author: z.string(),
+    tags: z.array(z.string()),
+    reads: z.array(z.object({
+        date_started: datestring,
+        date_finished: datestring,
+        review: z.string(),
+        rating: z.number().int().min(0).max(5), // 0 - dnf
+        tags: z.array(z.string())
+
+    }))
+})
+
+export const BooksType = z.array(BookType)
