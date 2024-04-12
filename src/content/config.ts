a799509729a6494c7b353b8b21f4f94b6061ae13
yinon <yinongilad4@gmail.com>
2024-04-12 18:27:30 +0300

    comment out art log db

--- src/content/config.ts
+++ src/content/config.ts
@@ -8,50 +8,50 @@ const blogCollection = defineCollection({
   }),
 });
 
-const Review = z.object({
-  date: z.string().datetime(), // get from github
-  body: z.optional(z.string()),
-  rating: z.optional(z.number().int().min(1).max(5)),
-})
+// const Review = z.object({
+//   date: z.string().datetime(), // get from github
+//   body: z.optional(z.string()),
+//   rating: z.optional(z.number().int().min(1).max(5)),
+// })
 
-const movieCollection = defineCollection({
-  type: "data",
-  schema: z.object({
-    title: z.string(),
-    url: z.optional(z.string().url()), // embed
-    watch_link: z.optional(z.string().url()),
-    tags: z.optional(z.array(z.string())),
-    views: z.optional(z.array(Review)),
-  })
-})
+// const movieCollection = defineCollection({
+//   type: "data",
+//   schema: z.object({
+//     title: z.string(),
+//     url: (z.string().url()).optional(), // embed // required?
+//     watch_link: (z.string().url()).optional(),
+//     tags: (z.array(z.string())).optional(),
+//     views: (z.array(Review)).optional(),
+//   })
+// })
 
-const bookCollection = defineCollection({
-  type: "data",
-  schema: z.object({
-    title: z.string(),
-    author: z.optional(z.string()), // if embed dont need
-    url: z.optional(z.string().url()), // embed
-    read_link: z.optional(z.string().url()),
-    tags: z.optional(z.array(z.string())),
-    reads: z.optional(z.array(Review)),
-  })
-})
+// const bookCollection = defineCollection({
+//   type: "data",
+//   schema: z.object({
+//     title: z.string(),
+//     author: (z.string()).optional(), // if embed dont need
+//     url: (z.string().url()).optional(), // embed // required?
+//     read_link: (z.string().url()).optional(),
+//     tags: (z.array(z.string())).optional(),
+//     reads: (z.array(Review)).optional(),
+//   })
+// })
 
-const musicCollection = defineCollection({
-  type: 'data',
-  schema: z.object({ // song or album
-    title: z.string(),
-    artist: z.string(), // if embed dont need
-    url: z.optional(z.string().url()), // embed
-    listen_link: z.optional(z.string().url()),
-    tags: z.optional(z.array(z.string())),
-    listens: z.optional(z.array(Review)),
-  })
-})
+// const musicCollection = defineCollection({
+//   type: 'data',
+//   schema: z.object({ // song or album
+//     title: z.string(),
+//     artist: z.string(), // if embed dont need
+//     url: z.string().url().optional(), // embed // required?
+//     listen_link: (z.string().url()).optional(),
+//     tags: (z.array(z.string())).optional(),
+//     listens: (z.array(Review)).optional(),
+//   })
+// })
 
 export const collections = {
   blog: blogCollection,
-  movies: movieCollection,
-  books: bookCollection,
-  music: musicCollection,
+  // movies: movieCollection,
+  // books: bookCollection,
+  // music: musicCollection,
 };
