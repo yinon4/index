838b05987e5d896f12eab219f01af01bf5dd8be5
yinon <yinongilad4@gmail.com>
2024-04-13 19:06:49 +0300

    movie filter, sort and styling

--- src/data/movies/functions.ts
+++ src/data/movies/functions.ts
@@ -11,3 +11,19 @@ export const getRating = (movie: Movie) => {
     return rating;
 }
 export const getTitle = ({ title }: Movie) => title.toLocaleLowerCase();
+export const getDate = ({ watches }: Movie) => {
+    if (!watches) return "";
+    return new Date(watches[watches.length - 1].date).toLocaleDateString('en-CA', { year: '2-digit', month: 'short' });
+}
+
+type sortFn = (a: Movie, b: Movie) => number;
+type filterFn = (movie: Movie) => boolean;
+export const sortMovies = (by: sortFn, movies: Movie[]) => movies.sort(by);
+export const filterMovies = (by: filterFn, movies: Movie[]) => movies.filter(by);
+
+export const byTitle: sortFn = (a, b) => {
+    return getTitle(a) > getTitle(b) ? 1 : -1;
+}
+
+export const bySeen: filterFn = (movie) => movie.watches?.length > 0;
+
