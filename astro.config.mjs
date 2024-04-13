e29635b0df09c3ec996ba44cc704dba4837d4590
yinon <yinongilad4@gmail.com>
2024-04-13 17:47:34 +0300

    upgrade version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  // integrations: [],
   site: 'https://yinon4.github.io',
   base: "/planet",
   trailingSlash: "ignore",
   devToolbar: {
-    enabled: false
+    enabled: true
   },
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,6 @@
-import { defineCollection, z } from "astro:content";
+import { z } from "astro/zod";
 
-const blogCollection = defineCollection({
+const blogCollection = ({
   type: "content",
   schema: z.object({
     title: z.string(),
@@ -8,50 +8,6 @@ const blogCollection = defineCollection({
   }),
 });
 
-// const Review = z.object({
-//   date: z.string().datetime(), // get from github
-//   body: z.optional(z.string()),
-//   rating: z.optional(z.number().int().min(1).max(5)),
-// })
-
-// const movieCollection = defineCollection({
-//   type: "data",
-//   schema: z.object({
-//     title: z.string(),
-//     url: (z.string().url()).optional(), // embed // required?
-//     watch_link: (z.string().url()).optional(),
-//     tags: (z.array(z.string())).optional(),
-//     views: (z.array(Review)).optional(),
-//   })
-// })
-
-// const bookCollection = defineCollection({
-//   type: "data",
-//   schema: z.object({
-//     title: z.string(),
-//     author: (z.string()).optional(), // if embed dont need
-//     url: (z.string().url()).optional(), // embed // required?
-//     read_link: (z.string().url()).optional(),
-//     tags: (z.array(z.string())).optional(),
-//     reads: (z.array(Review)).optional(),
-//   })
-// })
-
-// const musicCollection = defineCollection({
-//   type: 'data',
-//   schema: z.object({ // song or album
-//     title: z.string(),
-//     artist: z.string(), // if embed dont need
-//     url: z.string().url().optional(), // embed // required?
-//     listen_link: (z.string().url()).optional(),
-//     tags: (z.array(z.string())).optional(),
-//     listens: (z.array(Review)).optional(),
-//   })
-// })
-
 export const collections = {
   blog: blogCollection,
-  // movies: movieCollection,
-  // books: bookCollection,
-  // music: musicCollection,
 };
