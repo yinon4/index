71025d2bdea02a3fa8a5596134f94e24a903b6b7
yinon <yinongilad4@gmail.com>
2024-04-19 23:36:49 +0300

    add libby data

--- src/data/music/functions.ts
+++ src/data/music/functions.ts
@@ -1,18 +1,9 @@
-import { getLast } from "functions";
-import type { filter, get, sort } from "types";
+import type { get, sort } from "types";
 import { type Album } from "./types";
 
 export const getTitle: get<Album> = ({ title }) => title.toLocaleLowerCase();
 export const getArtist: get<Album> = ({ artist }) => artist.toLocaleLowerCase();
 
-// export const byTitle: sort<Album> = (a, b) => {
-//   return a.title.localeCompare(b.title);
-// };
-
-// export const bySeen: filter<Album> = (movie) => movie.watches?.length > 0;
-
-// export const byDate: sort<Album> = (a, b) => {
-//   const dateA = new Date(getLast(a.watches).date);
-//   const dateB = new Date(getLast(b.watches).date);
-//   return dateA.getTime() - dateB.getTime();
-// };
+export const byArtist: sort<Album> = (a, b) => {
+  return a.artist.localeCompare(b.artist);
+};
