8b77edd82b67d9d16eef30133d5d0c363c5ac8c5
yinon <yinongilad4@gmail.com>
2024-05-03 21:02:33 +0300

    chore: Refactor code for stricter type checking in Footer and Header components

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,12 +1,10 @@
 import { z } from "astro/zod";
 
-const blogCollection = {
-  type: "content",
-  schema: z.object({
-    title: z.string(),
-  }),
-};
-
 export const collections = {
-  blog: blogCollection,
+  blog: {
+    type: "content",
+    schema: z.object({
+      title: z.string(),
+    }),
+  },
 };
